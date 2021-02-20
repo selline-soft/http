9e0fc6cbbe8061f151ff867d7b3389d0b2a9fed0
Alexey Volkov <webwizardry@hotmail.com>
2021-02-20 15:15:21 +0300

    Реализация от https://github.com/Nyholm/psr7

--- src/Traits/MessageInternalTrait.php
+++ src/Traits/MessageInternalTrait.php
@@ -1,6 +1,7 @@
 <?php
 namespace Selline\Http\Traits;
 use Psr\Http\Message\StreamInterface;
+use Selline\Http\Stream;
 
 /**
  * Trait MessageInternalTrait
@@ -12,58 +13,191 @@ use Psr\Http\Message\StreamInterface;
  */
 trait MessageInternalTrait
 {
-    public function getProtocolVersion()
+    /** @var array Map of all registered headers, as original name => array of values */
+    private $headers = [];
+
+    /** @var array Map of lowercase header name => original name at registration */
+    private $headerNames = [];
+
+    /** @var string */
+    private $protocol = '1.1';
+
+    /** @var StreamInterface|null */
+    private $stream;
+
+    public function getProtocolVersion(): string
     {
-        // TODO: Implement getProtocolVersion() method.
+        return $this->protocol;
     }
 
-    public function withProtocolVersion($version)
+    public function withProtocolVersion($version): self
     {
-        // TODO: Implement withProtocolVersion() method.
+        if ($this->protocol === $version) {
+            return $this;
+        }
+
+        $new = clone $this;
+        $new->protocol = $version;
+
+        return $new;
     }
 
-    public function getHeaders()
+    public function getHeaders(): array
     {
-        // TODO: Implement getHeaders() method.
+        return $this->headers;
     }
 
-    public function hasHeader($name)
+    public function hasHeader($header): bool
     {
-        // TODO: Implement hasHeader() method.
+        return isset($this->headerNames[\strtr($header, 'ABCDEFGHIJKLMNOPQRSTUVWXYZ', 'abcdefghijklmnopqrstuvwxyz')]);
     }
 
-    public function getHeader($name)
+    public function getHeader($header): array
     {
-        // TODO: Implement getHeader() method.
+        $header = \strtr($header, 'ABCDEFGHIJKLMNOPQRSTUVWXYZ', 'abcdefghijklmnopqrstuvwxyz');
+        if (!isset($this->headerNames[$header])) {
+            return [];
+        }
+
+        $header = $this->headerNames[$header];
+
+        return $this->headers[$header];
     }
 
-    public function getHeaderLine($name)
+    public function getHeaderLine($header): string
     {
-        // TODO: Implement getHeaderLine() method.
+        return \implode(', ', $this->getHeader($header));
     }
 
-    public function withHeader($name, $value)
+    public function withHeader($header, $value): self
     {
-        // TODO: Implement withHeader() method.
+        $value = $this->validateAndTrimHeader($header, $value);
+        $normalized = \strtr($header, 'ABCDEFGHIJKLMNOPQRSTUVWXYZ', 'abcdefghijklmnopqrstuvwxyz');
+
+        $new = clone $this;
+        if (isset($new->headerNames[$normalized])) {
+            unset($new->headers[$new->headerNames[$normalized]]);
+        }
+        $new->headerNames[$normalized] = $header;
+        $new->headers[$header] = $value;
+
+        return $new;
     }
 
-    public function withAddedHeader($name, $value)
+    public function withAddedHeader($header, $value): self
     {
-        // TODO: Implement withAddedHeader() method.
+        if (!\is_string($header) || '' === $header) {
+            throw new \InvalidArgumentException('Header name must be an RFC 7230 compatible string.');
+        }
+
+        $new = clone $this;
+        $new->setHeaders([$header => $value]);
+
+        return $new;
     }
 
-    public function withoutHeader($name)
+    public function withoutHeader($header): self
     {
-        // TODO: Implement withoutHeader() method.
+        $normalized = \strtr($header, 'ABCDEFGHIJKLMNOPQRSTUVWXYZ', 'abcdefghijklmnopqrstuvwxyz');
+        if (!isset($this->headerNames[$normalized])) {
+            return $this;
+        }
+
+        $header = $this->headerNames[$normalized];
+        $new = clone $this;
+        unset($new->headers[$header], $new->headerNames[$normalized]);
+
+        return $new;
     }
 
-    public function getBody()
+    public function getBody(): StreamInterface
     {
-        // TODO: Implement getBody() method.
+        if (null === $this->stream) {
+            $this->stream = Stream::create('');
+        }
+
+        return $this->stream;
     }
 
-    public function withBody(StreamInterface $body)
+    public function withBody(StreamInterface $body): self
     {
-        // TODO: Implement withBody() method.
+        if ($body === $this->stream) {
+            return $this;
+        }
+
+        $new = clone $this;
+        $new->stream = $body;
+
+        return $new;
+    }
+
+    private function setHeaders(array $headers): void
+    {
+        foreach ($headers as $header => $value) {
+            if (\is_int($header)) {
+                // If a header name was set to a numeric string, PHP will cast the key to an int.
+                // We must cast it back to a string in order to comply with validation.
+                $header = (string) $header;
+            }
+            $value = $this->validateAndTrimHeader($header, $value);
+            $normalized = \strtr($header, 'ABCDEFGHIJKLMNOPQRSTUVWXYZ', 'abcdefghijklmnopqrstuvwxyz');
+            if (isset($this->headerNames[$normalized])) {
+                $header = $this->headerNames[$normalized];
+                $this->headers[$header] = \array_merge($this->headers[$header], $value);
+            } else {
+                $this->headerNames[$normalized] = $header;
+                $this->headers[$header] = $value;
+            }
+        }
+    }
+
+    /**
+     * Убедитесь, что заголовок соответствует RFC 7230.
+     *
+     * Имена заголовков должны быть непустой строкой, состоящей из токен-символов.
+     *
+     * Значения заголовка должны быть строками, состоящими из видимых символов со всеми необязательными
+     * начальные и конечные пробелы удалены. Этот метод всегда удаляет такие
+     * необязательный пробел. Обратите внимание, что метод не позволяет складывать пробелы внутри
+     * значения, поскольку это было объявлено устаревшим почти для всех случаев в соответствии с RFC.
+     *
+     * header-field  = field-name ":" OWS значение поля  OWS
+     * field-name    = 1 * ("!" / "#" / "$" / "%" / "&" / "'" / "*" / "+" / "-" / "." / "^ "
+     *               / "_" / "" "/" | " / "~" /% x30-39 / (% x41-5A /% x61-7A))
+     * OWS           = * (SP / HTAB)
+     * значение поля = * ((% x21-7E /% x80-FF) [1 * (SP / HTAB) (% x21-7E /% x80-FF)])
+     *
+     * @see https://tools.ietf.org/html/rfc7230#section-3.2.4
+     */
+    private function validateAndTrimHeader($header, $values): array
+    {
+        if (!\is_string($header) || 1 !== \preg_match("@^[!#$%&'*+.^_`|~0-9A-Za-z-]+$@", $header)) {
+            throw new \InvalidArgumentException('Header name must be an RFC 7230 compatible string.');
+        }
+
+        if (!\is_array($values)) {
+            // This is simple, just one value.
+            if ((!\is_numeric($values) && !\is_string($values)) || 1 !== \preg_match("@^[ \t\x21-\x7E\x80-\xFF]*$@", (string) $values)) {
+                throw new \InvalidArgumentException('Header values must be RFC 7230 compatible strings.');
+            }
+
+            return [\trim((string) $values, " \t")];
+        }
+
+        if (empty($values)) {
+            throw new \InvalidArgumentException('Header values must be a string or an array of strings, empty array given.');
+        }
+
+        // Assert Non empty array
+        $returnValues = [];
+        foreach ($values as $v) {
+            if ((!\is_numeric($v) && !\is_string($v)) || 1 !== \preg_match("@^[ \t\x21-\x7E\x80-\xFF]*$@", (string) $v)) {
+                throw new \InvalidArgumentException('Header values must be RFC 7230 compatible strings.');
+            }
+
+            $returnValues[] = \trim((string) $v, " \t");
+        }
+
+        return $returnValues;
     }
 }
